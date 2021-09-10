ecaa19c584b9856e4c086e227842fd2064db7a58
Abbie <77949813+Abbie-Bowen@users.noreply.github.com>
2021-09-10 10:55:38 -0700

    test image

--- src/components/MiniCard.js
+++ src/components/MiniCard.js
@@ -3,8 +3,9 @@ import React, {Component} from "react";
 class MiniCard extends Component {
     render() {
         return(
-            <div>
-                Hello!
+            <div className="mini">
+                <img className="mini-img" src="/public/media/images/sir-manuel-4P831Njlg6g-unsplash.jpg" alt="child talking"></img>
+                <p className="mini-text">/th/</p>
             </div>
         );
     }
